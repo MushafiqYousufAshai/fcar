efc04c4886dcb247835dbc89cc4fb3b2fbd443cf
Mushafiq Yousuf Ashai <mushafiqyousufashai@gmail.com>
2021-01-24 14:00:12 +0400

    Editing with admin side

--- pages/admin.py
+++ pages/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
-
+from .models import Teams
+from django.utils.html import format_html
 # Register your models here.
+
+
+class TeamAdmin(admin.ModelAdmin):
+
+    def thumbnail(self, object):
+        return format_html('<img src="{}" width="50" style="border-radius:50px" />'.format(object.photo.url))
+
+    thumbnail.short_description = 'Photo'
+    list_display = ('id', 'thumbnail', 'first_name', 'designation', 'created_date')
+    list_display_links = ('first_name', 'thumbnail', 'id')
+    search_fields = ('first_name', 'designation',)
+    list_filter = ('created_date',)
+
+
+admin.site.register(Teams, TeamAdmin)
+
